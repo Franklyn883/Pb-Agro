45da1129beaa36d31b7359ff086750b342c63a5f
Frank Alimimian <alimimianfrank@gmail.com>
2023-05-10 10:11:23 +0100

    Add navigation section

--- PbAgro/settings.py
+++ PbAgro/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'farmreach.apps.FarmreachConfig'
+    'farmreach.apps.FarmreachConfig',
+    
+    
 ]
 
 MIDDLEWARE = [
--- farmreach/urls.py
+++ farmreach/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpattherns = [
-    
+app_name = 'farmreach'
+
+urlpatterns = [
+    path('', views.home),
 ]
